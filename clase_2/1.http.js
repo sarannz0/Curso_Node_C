30f80264ce3eba1969828f3f8b930696d698086e
Sara Nuñez <balaguera2008gmail.com>
2026-04-13 11:43:31 -0500

    feat: :sparkles: Creacion de la api con pokemon

--- clase_2/1.http.js
+++ clase_2/1.http.js
@@ -1,20 +1,34 @@
 const http = require('node:http') // protocolo HTTP
 
+const fs = require('node:fs')
+
 const desiredPort = process.env.PORT ?? 1234
 
-const processRequest = http.createServer((req, res) => {
+const processRequest = (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
     if (req.url === '/') {
-        res.statusCode = 200 //OK
-        res.setHeader('Content-Type', 'application/json')
-        res.end('HBienvenido a mi pagina de Inicio')
+        res.end('<h1>Bienvenido a mi pagina de Inicio</h1>')
+    } else if (req.url === '/descarga-node.png') {
+        fs.readFile('./node-js.png', (err, data) => {
+            if (err) {
+              res.statusCode = 500
+              res.end('<h1>500 Internal Server Error</h1>')
+            } else {
+                res.setHeader('Content-Type', 'image/png')
+                res.end(data)
+            }
+        })
     } else if (req.url === '/contacto') {
-        res.statusCode = 200 //OK   
-        res.setHeader('Content-Type', 'text/plain; charset=utf-8')
         res.end('<h1>Contacto</h1>')
+    } else {
+        res.statusCode = 404 // Not Found
+        res.end('<h1>404</h1>')
+    }
 }
 
 const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
-        console.log('server listening on port http://localhost:${desiredPort}')
+    console.log('server listening on port http://localhost:${desiredPort}')
 })
